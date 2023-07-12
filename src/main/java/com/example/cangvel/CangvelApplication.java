4f6ae6974830b053e8b077a17c0d3456360fe869
lukrzak <lukrzak@wp.pl>
2023-07-12 19:05:40 +0200

    Initial commit

---
+++ src/main/java/com/example/cangvel/CangvelApplication.java
@@ -0,0 +1,13 @@
+package com.example.cangvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CangvelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CangvelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cangvel/CangvelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cangvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CangvelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
