1dd3eef1c9e3c0dd90c5facccd29149c24b69ce6
lukrzak <lukrzak@wp.pl>
2023-07-18 19:33:24 +0200

    Changed CvEvaluation to be record instead class

--- src/main/java/com/cangvel/models/CvEvaluation.java
+++ src/main/java/com/cangvel/models/CvEvaluation.java
@@ -1,22 +1,11 @@
 package com.cangvel.models;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.Set;
 
-@Getter
-@Setter
-public class CvEvaluation {
-    private float requirementFulfillment;
-    private boolean isAccepted;
-    private final Set<String> foundRequiredKeywords;
-    private final Set<String> foundOptionalKeywords;
-
-    public CvEvaluation(float acceptanceThreshold, float requirementFulfillment, Set<String> foundRequiredKeywords, Set<String> foundOptionalKeywords) {
-        this.requirementFulfillment = requirementFulfillment;
-        this.foundRequiredKeywords = foundRequiredKeywords;
-        this.foundOptionalKeywords = foundOptionalKeywords;
-        this.isAccepted = requirementFulfillment >= acceptanceThreshold;
-    }
+public record CvEvaluation(
+        float requirementFulfillment,
+        boolean isAccepted,
+        Set<String>foundRequiredKeywords,
+        Set<String> foundOptionalKeywords
+) {
 }
